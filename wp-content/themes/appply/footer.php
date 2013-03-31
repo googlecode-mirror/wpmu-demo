0c42137e07b727d05ad97c0a70de285dea80d53c
dashzaki <dashzaki@gmail.com>
2013-03-31 06:36:54 +0000

    Add contact information

--- wp-content/themes/appply/footer.php
+++ wp-content/themes/appply/footer.php
@@ -44,32 +44,49 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 <?php } // End IF Statement ?>
 		<footer id="footer" class="col-full">
 
-				<div id="footer-left" class="col-left">
+<!--            default theme-->
+<!--			    <div id="footer-left" class="col-left">-->
+<!---->
+<!--				--><?php //if( isset( $woo_options['woo_footer_left'] ) && $woo_options['woo_footer_left'] == 'true' ) {
+//						echo stripslashes( $woo_options['woo_footer_left_text'] );
+//				} else { ?>
+<!--					<p class="copyright">--><?php //bloginfo(); ?><!-- &copy; --><?php //echo date( 'Y' ); ?><!--. --><?php //_e( 'All Rights Reserved.', 'woothemes' ); ?><!--</p>-->
+<!--				--><?php //} ?>
+<!---->
+<!--				--><?php //if ( function_exists( 'has_nav_menu' ) && has_nav_menu( 'footer-menu' ) ) {
+//					wp_nav_menu( array( 'depth' => 1, 'sort_column' => 'menu_order', 'container' => 'ul', 'menu_id' => 'footer-nav', 'menu_class' => 'nav', 'theme_location' => 'footer-menu' ) );
+//				} ?>
+<!---->
+<!--		        --><?php //if( isset( $woo_options['woo_footer_right'] ) && $woo_options['woo_footer_right'] == 'true' ) {
+//		        	echo stripslashes( $woo_options['woo_footer_right_text'] );
+//				} else { ?>
+<!--					<p class="credit">--><?php //_e( 'Powered by', 'woothemes' ); ?><!-- <a href="--><?php //echo esc_url( 'http://www.mafiashare.net' ); ?><!--">WordPress</a>. --><?php //_e( 'Designed by', 'woothemes' ); ?><!-- <a href="http://mobtel.ro"><img src="--><?php //echo esc_url( get_template_directory_uri() . '/images/woothemes.png' ); ?><!--" width="74" height="19" alt="telefoane mobile" /></a></p>-->
+<!--				--><?php //} ?><!-- -->
+<!---->
+<!--				</div>-->
 
-				<?php if( isset( $woo_options['woo_footer_left'] ) && $woo_options['woo_footer_left'] == 'true' ) {
-						echo stripslashes( $woo_options['woo_footer_left_text'] );
-				} else { ?>
-					<p class="copyright"><?php bloginfo(); ?> &copy; <?php echo date( 'Y' ); ?>. <?php _e( 'All Rights Reserved.', 'woothemes' ); ?></p>
-				<?php } ?>
+<!--            New custom-->
+            <div id="footer-left" class="col-left">
+                <img src="<?php echo esc_url( get_template_directory_uri() . '/images/logo_gray_03.png' ); ?>"  />
+                <p></p>
+                <p class="copyright ">ซอยเอกชัย 83/1 แขวงบางบอน</p>
+                <p class="copyright ">เขตบางบอน กทม 10150น</p>
+                <p class="copyright ">Tel: 02-867-3888ม 02-867-380-04</p>
+                <p class="copyright ">Fax: 02-899-4994น</p>
+                <p class="copyright ">Email : sale_group@srikrungbroker.co.th</p>
 
-				<?php if ( function_exists( 'has_nav_menu' ) && has_nav_menu( 'footer-menu' ) ) {
-					wp_nav_menu( array( 'depth' => 1, 'sort_column' => 'menu_order', 'container' => 'ul', 'menu_id' => 'footer-nav', 'menu_class' => 'nav', 'theme_location' => 'footer-menu' ) );
-				} ?>
+            </div>
 
-		        <?php if( isset( $woo_options['woo_footer_right'] ) && $woo_options['woo_footer_right'] == 'true' ) {
-		        	echo stripslashes( $woo_options['woo_footer_right_text'] );
-				} else { ?>
-					<p class="credit"><?php _e( 'Powered by', 'woothemes' ); ?> <a href="<?php echo esc_url( 'http://www.mafiashare.net' ); ?>">WordPress</a>. <?php _e( 'Designed by', 'woothemes' ); ?> <a href="http://mobtel.ro"><img src="<?php echo esc_url( get_template_directory_uri() . '/images/woothemes.png' ); ?>" width="74" height="19" alt="telefoane mobile" /></a></p>
-				<?php } ?>
+            <divid=footer-center>
 
+            </div>
+
+			<div id="footer-right" class="col-right">
+				<div class="block">
+			        <?php woo_sidebar( 'footer-right' ); ?>
+                       <iframe src="//www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fplatform&amp;width=292&amp;height=290&amp;show_faces=true&amp;colorscheme=light&amp;stream=false&amp;border_color&amp;header=true&amp;appId=193435037371900" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:292px; height:290px; background-color: #ffffff" allowTransparency="false"></iframe>
 				</div>
-
-				<div id="footer-right" class="col-right">
-					<div class="block">
-			        	<?php woo_sidebar( 'footer-right' ); ?>
-                        <iframe src="//www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fplatform&amp;width=292&amp;height=290&amp;show_faces=true&amp;colorscheme=light&amp;stream=false&amp;border_color&amp;header=true&amp;appId=193435037371900" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:292px; height:290px; background-color: #ffffff" allowTransparency="false"></iframe>
-					</div>
-				</div>
+			</div>
 
 		</footer><!-- /#footer  -->
 
