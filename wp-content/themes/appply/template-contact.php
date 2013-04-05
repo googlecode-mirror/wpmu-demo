7f0f9911593b1199f45d41ebb7e29cce7c2b3fe0
dashzaki <dashzaki@gmail.com>
2013-04-05 12:25:16 +0000

    * Custom contact layout page.

--- wp-content/themes/appply/template-contact.php
+++ wp-content/themes/appply/template-contact.php
@@ -131,81 +131,84 @@ jQuery(document).ready(function() {
 
                 <?php while ( have_posts() ) { the_post(); ?>
 
-                        <section class="entry">
-	                        <?php the_content(); ?>
-                			
-                			<div class="location-twitter fix">
-    							<?php if ( isset( $woo_options['woo_contact_panel'] ) && $woo_options['woo_contact_panel'] == 'true' ) { ?>
-						    	<section id="office-location"<?php if ( ( isset( $woo_options['woo_contact_twitter'] ) && $woo_options['woo_contact_twitter'] != '' ) || ( isset($woo_options['woo_contact_subscribe_and_connect']) && $woo_options['woo_contact_subscribe_and_connect'] == 'true' ) ) { ?> class="col-left"<?php } ?>>
-									<?php if (isset($woo_options['woo_contact_title'])) { ?><h3><?php echo esc_html( $woo_options['woo_contact_title'] ); ?></h3><?php } ?>
-									<ul>
-										<?php if (isset($woo_options['woo_contact_title']) && $woo_options['woo_contact_title'] != '' ) { ?><li><?php echo nl2br( esc_html( $woo_options['woo_contact_address'] ) ); ?></li><?php } ?>
-										<?php if (isset($woo_options['woo_contact_number']) && $woo_options['woo_contact_number'] != '' ) { ?><li><?php _e('Tel:','woothemes'); ?> <?php echo esc_html( $woo_options['woo_contact_number'] ); ?></li><?php } ?>
-										<?php if (isset($woo_options['woo_contact_fax']) && $woo_options['woo_contact_fax'] != '' ) { ?><li><?php _e('Fax:','woothemes'); ?> <?php echo esc_html( $woo_options['woo_contact_fax'] ); ?></li><?php } ?>
-										<?php if (isset($woo_options['woo_contactform_email']) && $woo_options['woo_contactform_email'] != '' ) { ?><li><?php _e('Email:','woothemes'); ?> <a href="mailto:<?php echo esc_attr( $woo_options['woo_contactform_email'] ); ?>"><?php echo esc_html( $woo_options['woo_contactform_email'] ); ?></a></li><?php } ?>
-									</ul>
-						    	</section>ทฟย
-						    	<?php } ?>
-						    	<div class="contact-social<?php if ( ( isset( $woo_options['woo_contact_panel'] ) && $woo_options['woo_contact_panel'] == 'true' ) && ( ( isset( $woo_options['woo_contact_twitter'] ) && $woo_options['woo_contact_twitter'] != '' ) || ( isset($woo_options['woo_contact_subscribe_and_connect']) && $woo_options['woo_contact_subscribe_and_connect'] == 'true' ) ) ) { ?> col-right<?php } ?>">
-						    	
-						    		<?php if ( isset( $woo_options['woo_contact_twitter'] ) && $woo_options['woo_contact_twitter'] != '' ) { ?>
-						    		<section id="twitter">
-						    			<h3>Twitter</h3>
-						    			<ul id="twitter_update_list_123"><li></li></ul>
-						    			<?php echo woo_twitter_script(123, $woo_options['woo_contact_twitter'],1); ?>
-						    		</section>
-						    		<?php } ?>
-						    		<?php if ( isset($woo_options['woo_contact_subscribe_and_connect']) && $woo_options['woo_contact_subscribe_and_connect'] == 'true' ) { woo_subscribe_connect(); } ?>
-						    	
-						    	</div>
-						    	
-						    	</div><!-- /.location-twitter -->
-	                        
-                        </section>
+                            <?php if( isset( $hasError ) || isset( $captchaError ) ) { ?>
+                                <p class="alert"><?php _e( 'There was an error submitting the form.', 'woothemes' ); ?></p>
+                            <?php } ?>
+
+                            <?php if ( get_option( 'woo_contactform_email' ) == '' ) { ?>
+                                <?php echo do_shortcode( '[box type="alert"]' . __( 'E-mail has not been setup properly. Please add your contact e-mail!', 'woothemes' ) . '[/box]' );  ?>
+                            <?php } ?>
+
+
+                            <form action="<?php the_permalink(); ?>" id="contactForm" method="post">
+
+                                <ol class="forms">
+                                    <li><label for="contactName"><?php _e( 'Name', 'woothemes' ); ?></label>
+                                        <input type="text" name="contactName" id="contactName" value="<?php if( isset( $_POST['contactName'] ) ) { echo esc_attr( $_POST['contactName'] ); } ?>" class="txt requiredField" />
+                                        <?php if($nameError != '') { ?>
+                                            <span class="error"><?php echo $nameError;?></span>
+                                        <?php } ?>
+                                    </li>
+
+                                    <li><label for="email"><?php _e( 'Email', 'woothemes' ); ?></label>
+                                        <input type="text" name="email" id="email" value="<?php if( isset( $_POST['email'] ) ) { echo esc_attr( $_POST['email'] ); } ?>" class="txt requiredField email" />
+                                        <?php if($emailError != '') { ?>
+                                            <span class="error"><?php echo $emailError;?></span>
+                                        <?php } ?>
+                                    </li>
+
+                                    <li class="textarea"><label for="commentsText"><?php _e( 'Message', 'woothemes' ); ?></label>
+                                        <textarea name="comments" id="commentsText" rows="10" cols="10" class="requiredField"><?php if( isset( $_POST['comments'] ) ) { echo esc_textarea( $_POST['comments'] ); } ?></textarea>
+                                        <?php if( $commentError != '' ) { ?>
+                                            <span class="error"><?php echo $commentError; ?></span>
+                                        <?php } ?>
+                                    </li>
+                                    <li class="inline"><input type="checkbox" name="sendCopy" id="sendCopy" value="true"<?php if( isset( $_POST['sendCopy'] ) && $_POST['sendCopy'] == true ) { echo ' checked="checked"'; } ?> /><label for="sendCopy"><?php _e( 'Send a copy of this email to yourself', 'woothemes' ); ?></label></li>
+                                    <li class="screenReader"><label for="checking" class="screenReader"><?php _e( 'If you want to submit this form, do not enter anything in this field', 'woothemes' ); ?></label><input type="text" name="checking" id="checking" class="screenReader" value="<?php if( isset( $_POST['checking'] ) ) { echo esc_attr( $_POST['checking'] ); } ?>" /></li>
+                                    <li class="buttons"><input type="hidden" name="submitted" id="submitted" value="true" /><input class="submit button" type="submit" value="<?php esc_attr_e( 'Submit', 'woothemes' ); ?>" /></li>
+                                </ol>
+                            </form>
                         
                         <?php if ( isset($woo_options['woo_contactform_map_coords']) && $woo_options['woo_contactform_map_coords'] != '' ) { $geocoords = $woo_options['woo_contactform_map_coords']; }  else { $geocoords = ''; } ?>
                 		<?php if ($geocoords != '') { ?>
                 		<?php woo_maps_contact_output("geocoords=$geocoords"); ?>
-                		<?php echo do_shortcode( '[hr]' ); ?>
+<!--                		--><?php //echo do_shortcode( '[hr]' ); ?>
                 		<?php } ?>
 
-                    <?php if( isset( $hasError ) || isset( $captchaError ) ) { ?>
-                        <p class="alert"><?php _e( 'There was an error submitting the form.', 'woothemes' ); ?></p>
-                    <?php } ?>
+                            <section class="entry">
+                                <?php the_content(); ?>
 
-                    <?php if ( get_option( 'woo_contactform_email' ) == '' ) { ?>
-                        <?php echo do_shortcode( '[box type="alert"]' . __( 'E-mail has not been setup properly. Please add your contact e-mail!', 'woothemes' ) . '[/box]' );  ?>
-                    <?php } ?>
+                                <div class="location-twitter fix">
+                                    <?php if ( isset( $woo_options['woo_contact_panel'] ) && $woo_options['woo_contact_panel'] == 'true' ) { ?>
+                                        <section id="office-location"<?php if ( ( isset( $woo_options['woo_contact_twitter'] ) && $woo_options['woo_contact_twitter'] != '' ) || ( isset($woo_options['woo_contact_subscribe_and_connect']) && $woo_options['woo_contact_subscribe_and_connect'] == 'true' ) ) { ?> class="col-left"<?php } ?>>
+<!--                                            --><?php //if (isset($woo_options['woo_contact_title'])) { ?><!--<h3>--><?php //echo esc_html( $woo_options['woo_contact_title'] ); ?><!--</h3>--><?php //} ?>
+                                            <br>
+                                            <ul>
+                                                <?php if (isset($woo_options['woo_contact_title']) && $woo_options['woo_contact_title'] != '' ) { ?><li><?php echo ( esc_html( $woo_options['woo_contact_address'] ) ); ?></li><?php } ?>
+                                                <?php if (isset($woo_options['woo_contact_number']) && $woo_options['woo_contact_number'] != '' ) { ?><li><?php _e('Tel:','woothemes'); ?> <?php echo esc_html( $woo_options['woo_contact_number'] ); ?></li><?php } ?>
+                                                <?php if (isset($woo_options['woo_contact_fax']) && $woo_options['woo_contact_fax'] != '' ) { ?><li><?php _e('Fax:','woothemes'); ?> <?php echo esc_html( $woo_options['woo_contact_fax'] ); ?></li><?php } ?>
+                                                <?php if (isset($woo_options['woo_contactform_email']) && $woo_options['woo_contactform_email'] != '' ) { ?><li><?php _e('Email:','woothemes'); ?> <a href="mailto:<?php echo esc_attr( $woo_options['woo_contactform_email'] ); ?>"><?php echo esc_html( $woo_options['woo_contactform_email'] ); ?></a></li><?php } ?>
+                                            </ul>
+                                        </section>ทฟย
+                                    <?php } ?>
+                                    <div class="contact-social<?php if ( ( isset( $woo_options['woo_contact_panel'] ) && $woo_options['woo_contact_panel'] == 'true' ) && ( ( isset( $woo_options['woo_contact_twitter'] ) && $woo_options['woo_contact_twitter'] != '' ) || ( isset($woo_options['woo_contact_subscribe_and_connect']) && $woo_options['woo_contact_subscribe_and_connect'] == 'true' ) ) ) { ?> col-right<?php } ?>">
+
+                                        <?php if ( isset( $woo_options['woo_contact_twitter'] ) && $woo_options['woo_contact_twitter'] != '' ) { ?>
+                                            <section id="twitter">
+                                                <h3>Twitter</h3>
+                                                <ul id="twitter_update_list_123"><li></li></ul>
+                                                <?php echo woo_twitter_script(123, $woo_options['woo_contact_twitter'],1); ?>
+                                            </section>
+                                        <?php } ?>
+                                        <?php if ( isset($woo_options['woo_contact_subscribe_and_connect']) && $woo_options['woo_contact_subscribe_and_connect'] == 'true' ) { woo_subscribe_connect(); } ?>
+
+                                    </div>
+
+                                </div><!-- /.location-twitter -->
+
+                            </section>
 
 
-                    <form action="<?php the_permalink(); ?>" id="contactForm" method="post">
-
-                        <ol class="forms">
-                            <li><label for="contactName"><?php _e( 'Name', 'woothemes' ); ?></label>
-                                <input type="text" name="contactName" id="contactName" value="<?php if( isset( $_POST['contactName'] ) ) { echo esc_attr( $_POST['contactName'] ); } ?>" class="txt requiredField" />
-                                <?php if($nameError != '') { ?>
-                                    <span class="error"><?php echo $nameError;?></span>
-                                <?php } ?>
-                            </li>
-
-                            <li><label for="email"><?php _e( 'Email', 'woothemes' ); ?></label>
-                                <input type="text" name="email" id="email" value="<?php if( isset( $_POST['email'] ) ) { echo esc_attr( $_POST['email'] ); } ?>" class="txt requiredField email" />
-                                <?php if($emailError != '') { ?>
-                                    <span class="error"><?php echo $emailError;?></span>
-                                <?php } ?>
-                            </li>
-
-                            <li class="textarea"><label for="commentsText"><?php _e( 'Message', 'woothemes' ); ?></label>
-                                <textarea name="comments" id="commentsText" rows="10" cols="10" class="requiredField"><?php if( isset( $_POST['comments'] ) ) { echo esc_textarea( $_POST['comments'] ); } ?></textarea>
-                                <?php if( $commentError != '' ) { ?>
-                                    <span class="error"><?php echo $commentError; ?></span>
-                                <?php } ?>
-                            </li>
-                            <li class="inline"><input type="checkbox" name="sendCopy" id="sendCopy" value="true"<?php if( isset( $_POST['sendCopy'] ) && $_POST['sendCopy'] == true ) { echo ' checked="checked"'; } ?> /><label for="sendCopy"><?php _e( 'Send a copy of this email to yourself', 'woothemes' ); ?></label></li>
-                            <li class="screenReader"><label for="checking" class="screenReader"><?php _e( 'If you want to submit this form, do not enter anything in this field', 'woothemes' ); ?></label><input type="text" name="checking" id="checking" class="screenReader" value="<?php if( isset( $_POST['checking'] ) ) { echo esc_attr( $_POST['checking'] ); } ?>" /></li>
-                            <li class="buttons"><input type="hidden" name="submitted" id="submitted" value="true" /><input class="submit button" type="submit" value="<?php esc_attr_e( 'Submit', 'woothemes' ); ?>" /></li>
-                        </ol>
-                    </form>
 
                     <?php
                     		} // End WHILE Loop
