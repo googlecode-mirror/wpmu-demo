bd89863c3424258716aaf01b846f0a3f3df2ff44
thethak@gmail.com <thethak@gmail.com@6d25ae59-7d2f-b769-95da-61328136626c>
2013-05-11 20:08:19 +0000

    * add create webpage step page 1-4 * add some step page pitures

---
+++ trunk/wp-content/themes/appply/template-create-web-step2.php
@@ -0,0 +1,89 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit;
+/**
+ * Template Name: Create Website - Step 2
+ *
+ * @package WooFramework
+ * @subpackage Template
+ */
+
+ global $woo_options;
+ get_header();
+?>
+
+    <div id="content" class="page col-full">
+
+        <?php woo_main_before(); ?>
+
+        <section id="main" class="fullwidth">
+            <div id="create-web-progress-bar">
+                <div class="inline">
+                    <div class="step active">
+                        <span>1</span>
+                        <div class="step-text active">ยืนยันตนเอง</div>
+                    </div>
+
+                </div>
+                <div class="inline">
+                    <div class="bar active"></div>
+                    <div class="step active">
+                        <span>2</span>
+                        <div class="step-text active">กรอกข้อมูลติดต่อ</div>
+                    </div>
+
+                </div>
+                <div class="inline">
+                    <div class="bar"></div>
+                    <div class="step">
+                        <span>3</span>
+                        <div class="step-text">เลือกชื่อเว็บไซต์</div>
+                    </div>
+
+                </div>
+                <div class="inline">
+                    <div class="bar"></div>
+                    <div class="step">
+                        <span>4</span>
+                        <div class="step-text">ยืนยันข้อมูล</div>
+                    </div>
+                </div>
+            </div>
+
+            <div id="create-web-content">
+
+            </div>
+
+
+            
+            <?php
+            if ( have_posts() ) { $count = 0;
+                while ( have_posts() ) { the_post(); $count++;
+                    ?>
+                    <article <?php post_class(); ?>>
+
+                        <section class="entry">
+                            <?php the_content(); ?>
+                        </section><!-- /.entry -->
+
+                        <?php edit_post_link( __( '{ Edit }', 'woothemes' ), '<span class="small">', '</span>' ); ?>
+
+                    </article><!-- /.post -->
+
+                <?php
+                } // End WHILE Loop
+            }
+            else {
+                ?>
+                <article <?php post_class(); ?>>
+                    <p><?php _e( 'Sorry, no posts matched your criteria.', 'woothemes' ); ?></p>
+                </article><!-- /.post -->
+            <?php }
+            ?>
+
+        </section><!-- /#main -->
+
+        <?php woo_main_after(); ?>
+
+    </div><!-- /#content -->
+
+<?php get_footer(); ?>
