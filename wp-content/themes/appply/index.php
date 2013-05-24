9cfa5fea401563079c3136df13347d0573e4b037
dashzaki <dashzaki@gmail.com>
2013-05-24 06:38:08 +0000

    * Query featured product.

--- wp-content/themes/appply/index.php
+++ wp-content/themes/appply/index.php
@@ -34,7 +34,11 @@ if ( ! defined( 'ABSPATH' ) ) exit;
         <div id="product-recommend">
             <div class="product-slider">
                 <?php
-                $args = array('post_type' => 'product', 'posts_per_page' => 12);
+                $args = array('post_type' => 'product',
+                    'posts_per_page' => 12,
+                    'meta_key' => '_featured',
+                    'meta_value' => 'yes'
+                );
                 $loop = new WP_Query( $args );
                 if ( $loop->have_posts() ) {
                     while ( $loop->have_posts() ) : $loop->the_post(); ?>
