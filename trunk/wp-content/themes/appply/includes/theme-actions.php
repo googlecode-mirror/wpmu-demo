5107d311da8ae2ec9d2bf96d83165a858716bd72
thethak@gmail.com <thethak@gmail.com@6d25ae59-7d2f-b769-95da-61328136626c>
2013-06-21 11:19:00 +0000

    * add theme KAKIA for microweb

--- trunk/wp-content/themes/appply/includes/theme-actions.php
+++ trunk/wp-content/themes/appply/includes/theme-actions.php
@@ -106,22 +106,22 @@ if ( ! function_exists( 'woo_load_frontend_css' ) ) {
 /* Load responsive <meta> tags in the <head> */
 /*-----------------------------------------------------------------------------------*/
 
-//add_action( 'wp_head', 'woo_load_responsive_meta_tags', 10 );
-//
-//if ( ! function_exists( 'woo_load_responsive_meta_tags' ) ) {
-//	function woo_load_responsive_meta_tags () {
-//		$html = '';
-//
-//		$html .= "\n" . '<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->' . "\n";
-//		$html .= '<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />' . "\n";
-//
-//		/* Remove this if not responsive design */
-//		$html .= "\n" . '<!--  Mobile viewport scale | Disable user zooming as the layout is optimised -->' . "\n";
-//		$html .= '<meta content="initial-scale=1.0; maximum-scale=1.0; user-scalable=no" name="viewport"/>' . "\n";
-//
-//		echo $html;
-//	} // End woo_load_responsive_meta_tags()
-//}
+add_action( 'wp_head', 'woo_load_responsive_meta_tags', 10 );
+
+if ( ! function_exists( 'woo_load_responsive_meta_tags' ) ) {
+	function woo_load_responsive_meta_tags () {
+		$html = '';
+
+		$html .= "\n" . '<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->' . "\n";
+		$html .= '<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />' . "\n";
+
+		/* Remove this if not responsive design */
+		$html .= "\n" . '<!--  Mobile viewport scale | Disable user zooming as the layout is optimised -->' . "\n";
+		$html .= '<meta content="initial-scale=1.0; maximum-scale=1.0; user-scalable=no" name="viewport"/>' . "\n";
+
+		echo $html;
+	} // End woo_load_responsive_meta_tags()
+}
 
 /*-----------------------------------------------------------------------------------*/
 /* Add Custom Styling to HEAD */
